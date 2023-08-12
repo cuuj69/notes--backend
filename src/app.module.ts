b1e9b8846d36ed875a6d0624e5878e392c49653b
cuuj69 <mensahjefferson69@gmail.com>
2023-08-12 22:43:03 +0000

    Modified:restructured code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { NoteController } from './note/controllers/note.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, NoteController],
-  providers: [AppService],
+  controllers: [NoteController],
+  providers: [],
 })
 export class AppModule {}
